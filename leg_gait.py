984e9800613bb577226ecdf149bdffcadceec090
FinnGagliano <finngagliano30@gmail.com>
2019-08-07 14:46:04 +0100

    Try to reset servos into 0 degrees at end of script

--- leg_gait.py
+++ leg_gait.py
@@ -7,12 +7,13 @@ servo_pins = [17, 27]
 for servo_pin in servo_pins:
     pi.set_mode(servo_pin, pigpio.OUTPUT)
 
-positions = [[30, -30], [0,0]]
+positions = [[45, -45], [0,0]]
 
 def move_to_angle(pi, pin, angle):
     pwm_range = 2000
     duty_cycle = (angle / 180) * pwm_range + pwm_range / 2
     duty_cycle += 500
+    print(str(duty_cycle))
     pi.set_servo_pulsewidth(pin, duty_cycle)
 
 try:
