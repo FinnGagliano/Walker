f4b1b18ec2db19794f5d1363919c0eae8ad8abc7
FinnGagliano <finngagliano30@gmail.com>
2019-08-07 15:00:19 +0100

    Try to reset servos into 0 degrees at end of script

--- leg_gait.py
+++ leg_gait.py
@@ -22,6 +22,7 @@ try:
         # Each servo
         for j in range(2):
             move_to_angle(pi, servo_pins[j], positions[i][j])
+            time.sleep(0.2)
         time.sleep(5)
     for servo_pin in servo_pins:
         pi.set_servo_pulsewidth(servo_pin, 0)
