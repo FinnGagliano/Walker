7ac584fa4e88e48d8fbe57e2bb7c0c2a449d34a6
FinnGagliano <finngagliano30@gmail.com>
2019-08-07 14:15:53 +0100

    First attempt at moving into gait positions

---
+++ leg_gait.py
@@ -0,0 +1,29 @@
+import pigpio
+import time
+
+pi = pigpio.pi()
+servo_pins = [17, 27]
+
+for servo_pin in servo_pins:
+    pi.set_mode(servo_pin, pigpio.OUTPUT)
+
+positions = [[0,0], [30, -30]]
+
+def move_to_angle(pi, pin, angle):
+    pwm_range = 2000
+    duty_cycle = (angle / 90) * pwm_range + pwm_range / 2
+    duty_cycle += 500
+    pi.set_servo_pulsewidth(pin, duty_cycle)
+
+try:
+    # Each position
+    for i in range(2):
+        # Each servo
+        for j in range(2):
+            move_to_angle(pi, pin[j], position[i][j])
+        time.sleep(5)
+except:
+    for servo_pin in servo_pins:
+        pi.set_servo_pulsewidth(servoPIN, 0)
+    time.sleep(1)
+    pi.stop()
