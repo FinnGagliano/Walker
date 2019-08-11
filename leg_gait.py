bf4eaa0cbcccd171ffe57dde938fb5483c87fc8e
FinnGagliano <finngagliano30@gmail.com>
2019-08-11 14:59:00 +0100

    leg_gait

--- leg_gait.py
+++ leg_gait.py
@@ -41,7 +41,7 @@ try:
     while True:
         move_to_angle(pi, servo_pins[0], 45)
         time.sleep(5)
-        move_to_angle(pi, servo_pins[0], 45)
+        move_to_angle(pi, servo_pins[1], 45)
 
 except Exception as e:
     time.sleep(1)
